f59f63a71fd3318ff341894095dacbc8c7149c0a
Adam Balcerek <a.balcerek@adform.com>
2022-01-20 18:23:08 +0100

    internet gateway

--- create-stack.sh
+++ create-stack.sh
@@ -4,12 +4,25 @@ set -o allexport
 source stack.env
 set +o allexport
 
+
+rm -f ${key_pair_name}.pem
+
+
+aws ec2 delete-key-pair \
+  --key-name ${key-pair-name}
+
+key=$(aws ec2 create-key-pair \
+  --key-name ${key-pair-name} | jq .KeyMaterial )
+  
+echo ${key:1:-1} | sed 's/\\n/\n/g' > ${key_pair_name}.pem
+
 aws cloudformation describe-stacks \
   --stack-name ${stack_name} || \
 echo creating stack: && \
 aws cloudformation create-stack \
   --stack-name ${stack_name} \
-  --template-body file://stack.yaml
+  --template-body file://stack.yaml \
+  --parameters ParameterKey=KeyPairName,ParameterValue=${key_pair_name}
 
 aws cloudformation wait stack-exists \
   --stack-name ${stack_name}
--- update-stack.sh
+++ update-stack.sh
@@ -6,6 +6,8 @@ set +o allexport
 
 aws cloudformation update-stack \
   --stack-name ${stack_name} \
-  --template-body file://stack.yaml 
+  --template-body file://stack.yaml \
+  --parameters ParameterKey=KeyPairName,ParameterValue=${key_pair_name}
+
 
 echo "it is safe to exit script now. wating for stack to be updated"
